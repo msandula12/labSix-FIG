b61bde1c7830ab6495e30f5e4cc23a34ae90b815
Michael Sandula <msandula@gmail.com>
2016-03-30 18:01:06 -0400

    Accounted for no input or if input is not a number

--- figure.js
+++ figure.js
@@ -3,8 +3,10 @@ function rando(userNumber) {
 	var userNumber = document.getElementById("userNum").value;
 	var randomNumber = Math.ceil(Math.random() * 10);
 	var display = document.getElementById("displayMsg");
-	if (userNumber === randomNumber) {
-		display.innerHTML = "Good work";
+	if (!userNumber || isNaN(userNumber)) {
+		alert("Please enter a number!");
+	} else if (userNumber === randomNumber) {
+		display.innerHTML = "Good work!";
 	} else {
 		display.innerHTML = "Not a match ... Guess again!";
 	}
