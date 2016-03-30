04250951b3fb9fc2e1fda0a85d392c82d8d1f938
Michael Sandula <msandula@gmail.com>
2016-03-30 18:09:41 -0400

    Makes sure user number is between 1-10

--- figure.js
+++ figure.js
@@ -5,9 +5,13 @@ function rando(userNumber) {
 	var display = document.getElementById("displayMsg");
 	if (!userNumber || isNaN(userNumber)) {
 		alert("Please enter a number!");
+	} else if (userNumber > 10 || userNumber < 1) {
+		alert("Please enter a number between 1-10!")
 	} else if (userNumber === randomNumber) {
 		display.innerHTML = "Good work!";
 	} else {
-		display.innerHTML = "Not a match ... Guess again!";
+		display.innerHTML = "Not a match ... Guess again!"
+		console.log(randomNumber);
 	}
+	document.getElementById("form").reset();
 }
